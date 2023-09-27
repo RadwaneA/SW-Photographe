d9e50d0aecfae28f282b68aed4740002006c95fa
Raloui <Radwane.aloui@ynov.com>
2023-09-27 19:08:21 +0200

    Indentation

--- js/projet.js
+++ js/projet.js
@@ -1,220 +1,220 @@
 // Variables globales
 let currentImageIndex = 0;
-const carousel = document.querySelector('.carousel');
+const carousel = document.querySelector(".carousel");
 const projects = [
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/coiffeuserousse.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/coiffeuserousse.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/backgroundwhite.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/backgroundwhite.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/barbumiroir.jpg",
-            category: "all, collaboration",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/barbumiroir.jpg",
+    category: "all, collaboration",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/beigededos.jpg",
-            category: "all, collaboration",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/beigededos.jpg",
+    category: "all, collaboration",
+  },
 
-            {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/blondebat.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/blondebat.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/blondesouriante.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/blondesouriante.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/bodybuilder.jpg",
-            category: "all, collaboration, shooting, evenementiel",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/bodybuilder.jpg",
+    category: "all, collaboration, shooting, evenementiel",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/doublefrere.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/doublefrere.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/locksbeige.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/locksbeige.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/locksviolet.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/locksviolet.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/locksvioletbat.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/locksvioletbat.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/locksvoiture.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/locksvoiture.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/misscoiffure.jpg",
-            category: "all, shooting, evenementiel",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/misscoiffure.jpg",
+    category: "all, shooting, evenementiel",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/misscoiffureprofile.jpg",
-            category: "all, shooting, evenementiel",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/misscoiffureprofile.jpg",
+    category: "all, shooting, evenementiel",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/peinturemoustachu.jpg",
-            category: "all, shooting, collaboration",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/peinturemoustachu.jpg",
+    category: "all, shooting, collaboration",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/promo.jpg",
-            category: "all, collaboration, promotion",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/promo.jpg",
+    category: "all, collaboration, promotion",
+  },
 
-            {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/roussededos.jpg",
-            category: "all, shooting, collaboration",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/roussededos.jpg",
+    category: "all, shooting, collaboration",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/samilumière.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/samilumière.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/saminoirblanc.jpg",
-            category: "all, shooting",
-        },
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/saminoirblanc.jpg",
+    category: "all, shooting",
+  },
 
-        {
-            name: "Nom du projet 1",
-            description: "Description courte du projet 1.",
-            details: "Description détaillée du projet 1.",
-            date: "XX/XX/XXXX",
-            technologies: "HTML, CSS, JavaScript",
-            imageSrc: "../images/tailleurlumiere.jpg",
-            category: "all, shooting, collaboration",
-        },
-    ];
+  {
+    name: "Nom du projet 1",
+    description: "Description courte du projet 1.",
+    details: "Description détaillée du projet 1.",
+    date: "XX/XX/XXXX",
+    technologies: "HTML, CSS, JavaScript",
+    imageSrc: "../images/tailleurlumiere.jpg",
+    category: "all, shooting, collaboration",
+  },
+];
 // Fonction pour afficher une image spécifique du carrousel
 function showImage(index) {
-    const translateX = -index * 100 + '%'; // Calcul de la valeur 'translateX'
-    carousel.style.transform = `translateX(${translateX})`;
+  const translateX = -index * 100 + "%"; // Calcul de la valeur 'translateX'
+  carousel.style.transform = `translateX(${translateX})`;
 }
 
 // Fonction pour afficher l'image suivante automatiquement
 function showNextImage() {
-    currentImageIndex++;
-    if (currentImageIndex >= portfolioItems.length) {
-        currentImageIndex = 0;
-    }
-    showImage(currentImageIndex);
+  currentImageIndex++;
+  if (currentImageIndex >= portfolioItems.length) {
+    currentImageIndex = 0;
+  }
+  showImage(currentImageIndex);
 }
 
 // Démarrez le défilement automatique toutes les X millisecondes (par exemple, 3000 ms)
@@ -222,54 +222,54 @@ const autoScrollInterval = 3000; // Réglez l'intervalle en millisecondes
 let autoScrollTimer = setInterval(showNextImage, autoScrollInterval);
 
 // Arrêtez le défilement automatique lorsque la souris passe sur le carrousel
-carousel.addEventListener('mouseenter', () => {
-    clearInterval(autoScrollTimer);
+carousel.addEventListener("mouseenter", () => {
+  clearInterval(autoScrollTimer);
 });
 
 // Redémarrez le défilement automatique lorsque la souris quitte le carrousel
-carousel.addEventListener('mouseleave', () => {
-    autoScrollTimer = setInterval(showNextImage, autoScrollInterval);
+carousel.addEventListener("mouseleave", () => {
+  autoScrollTimer = setInterval(showNextImage, autoScrollInterval);
 });
 
 // Fonction pour gérer le clic sur le bouton "Suivant"
 function handleNextButtonClick() {
-    currentImageIndex++;
-    if (currentImageIndex >= portfolioItems.length) {
-        currentImageIndex = 0;
-    }
-    showImage(currentImageIndex);
+  currentImageIndex++;
+  if (currentImageIndex >= portfolioItems.length) {
+    currentImageIndex = 0;
+  }
+  showImage(currentImageIndex);
 }
 
 // Fonction pour gérer le clic sur le bouton "Précédent"
 function handlePrevButtonClick() {
-    currentImageIndex--;
-    if (currentImageIndex < 0) {
-        currentImageIndex = portfolioItems.length - 1;
-    }
-    showImage(currentImageIndex);
+  currentImageIndex--;
+  if (currentImageIndex < 0) {
+    currentImageIndex = portfolioItems.length - 1;
+  }
+  showImage(currentImageIndex);
 }
 
 // Attachez des gestionnaires d'événements aux boutons "Suivant" et "Précédent"
-const nextButton = document.getElementById('nextBtn');
-const prevButton = document.getElementById('prevBtn');
+const nextButton = document.getElementById("nextBtn");
+const prevButton = document.getElementById("prevBtn");
 
-nextButton.addEventListener('click', handleNextButtonClick);
-prevButton.addEventListener('click', handlePrevButtonClick);
+nextButton.addEventListener("click", handleNextButtonClick);
+prevButton.addEventListener("click", handlePrevButtonClick);
 
 // Fonction pour afficher les superpositions d'images au survol
 portfolioItems.forEach((item) => {
-    const imageOverlay = item.querySelector('.image-overlay');
-    const overlayContent = item.querySelector('.overlay-content');
+  const imageOverlay = item.querySelector(".image-overlay");
+  const overlayContent = item.querySelector(".overlay-content");
 
-    item.addEventListener('mouseenter', () => {
-        imageOverlay.style.opacity = 1;
-        imageOverlay.style.transform = 'translateY(0)';
-        overlayContent.style.opacity = 1;
-    });
+  item.addEventListener("mouseenter", () => {
+    imageOverlay.style.opacity = 1;
+    imageOverlay.style.transform = "translateY(0)";
+    overlayContent.style.opacity = 1;
+  });
 
-    item.addEventListener('mouseleave', () => {
-        imageOverlay.style.opacity = 0;
-        imageOverlay.style.transform = 'translateY(-100%)';
-        overlayContent.style.opacity = 0;
-    });
+  item.addEventListener("mouseleave", () => {
+    imageOverlay.style.opacity = 0;
+    imageOverlay.style.transform = "translateY(-100%)";
+    overlayContent.style.opacity = 0;
+  });
 });
--- js/script.js
+++ js/script.js
@@ -1,43 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Sélectionnez tous les boutons "Détails"
-    const detailsButtons = document.querySelectorAll('.details-button');
-    const modal = document.querySelector('.modal');
-    const closeModalButton = document.querySelector('.close-modal-button');
+document.addEventListener("DOMContentLoaded", () => {
+  // Sélectionnez tous les boutons "Détails"
+  const detailsButtons = document.querySelectorAll(".details-button");
+  const modal = document.querySelector(".modal");
+  const closeModalButton = document.querySelector(".close-modal-button");
 
-    // Fonction pour afficher un message de confirmation après l'envoi du formulaire de contact
-    const contactForm = document.querySelector('.form');
+  // Fonction pour afficher un message de confirmation après l'envoi du formulaire de contact
+  const contactForm = document.querySelector(".form");
 
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // Empêche l'envoi du formulaire par défaut
 
-    contactForm.addEventListener('submit', (e) => {
-        e.preventDefault(); // Empêche l'envoi du formulaire par défaut
+    // Vous pouvez ici ajouter une logique pour envoyer le formulaire au serveur (par exemple, via une requête AJAX)
 
-        // Vous pouvez ici ajouter une logique pour envoyer le formulaire au serveur (par exemple, via une requête AJAX)
+    // Afficher un message de confirmation
+    const confirmationMessage = document.createElement("p");
+    confirmationMessage.textContent = "Votre message a été envoyé avec succès!";
+    confirmationMessage.style.color = "green";
+    contactForm.appendChild(confirmationMessage);
 
-        // Afficher un message de confirmation
-        const confirmationMessage = document.createElement('p');
-        confirmationMessage.textContent = 'Votre message a été envoyé avec succès!';
-        confirmationMessage.style.color = 'green';
-        contactForm.appendChild(confirmationMessage);
+    // Réinitialiser le formulaire
+    setTimeout(() => {
+      confirmationMessage.remove(); // Supprime le message après quelques secondes
+      contactForm.reset();
+    }, 3000); // Supprime le message après 3 secondes (3000 millisecondes)
+  });
+  // JavaScript pour gérer le défilement de la page et l'expansion du header
+  const header = document.querySelector("header");
 
-        // Réinitialiser le formulaire
-        setTimeout(() => {
-            confirmationMessage.remove(); // Supprime le message après quelques secondes
-            contactForm.reset();
-        }, 3000); // Supprime le message après 3 secondes (3000 millisecondes)
-    });
-    // JavaScript pour gérer le défilement de la page et l'expansion du header
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
-        header.classList.add('expanded'); // Ajoute la classe "expanded" pour élargir le header
+      header.classList.add("expanded"); // Ajoute la classe "expanded" pour élargir le header
     } else {
-        header.classList.remove('expanded'); // Retire la classe "expanded" pour réduire le header
+      header.classList.remove("expanded"); // Retire la classe "expanded" pour réduire le header
     }
-});
+  });
 
-/* JavaScript pour mettre à jour l'indicateur de progression de défilement */
-document.addEventListener("scroll", function() {
+  /* JavaScript pour mettre à jour l'indicateur de progression de défilement */
+  document.addEventListener("scroll", function () {
     const scrollIndicator = document.querySelector(".indicator-fill");
     const windowHeight = window.innerHeight;
     const documentHeight = document.documentElement.scrollHeight;
@@ -45,7 +44,5 @@ document.addEventListener("scroll", function() {
 
     const scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
     scrollIndicator.style.width = scrollPercent + "%";
+  });
 });
-});
-
-
